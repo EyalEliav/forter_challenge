a3abf1604df12c9c6c530ed3e86ec5495ed18b6b
Eyal Eliav <eyaleliav15@gmail.com>
2020-05-27 15:09:29 +0300

    db files added

--- src/Question.java
+++ src/Question.java
@@ -1,5 +1,6 @@
 public class Question {
-    public static int countUniqueNames(String billFirstName,String billLastName,String shipFirstName,String shipLastName,String billNameOnCard){
+    public static int countUniqueNames(String firstName, String billFirstName, String billLastName, String shipFirstName, String shipLastName, String billNameOnCard){
+        LastNamesDb nameDB = new LastNamesDb("src/db/lastnames.csv");
         return 0;
     }
 }
